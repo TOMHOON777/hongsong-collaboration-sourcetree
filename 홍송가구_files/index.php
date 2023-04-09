41de20a04efd1b6ab5affd97771094ee7c2d2f24
DESKTOP-7JUBBI6\dhwjd <kmi5063@naver.com>
2023-04-09 12:57:56 +0900

    html 및 css,js 23/4/9 홈화면

---
+++ 홍송가구_files/index.php
@@ -0,0 +1,210 @@
+<html lang="ko" xml:lang="ko" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=euc-kr">
+<title>춘천인테리어필름</title>
+<meta name="robots" content="all">
+<meta name="generator" content="kimsQ-RB 1.2.2"> 
+<meta name="author" content="http://xn--jx2bxcv35bvwb81kgrbx9lkyi.com"> 
+<meta name="subject" content="춘천인테리어필름"> 
+<meta name="keywords" content="춘천인테리어필름,인테리어필름,시트지,리폼,시공,가구,싱크대,아파트,상가,모텔,사무실,현관문,방문,샷시,홍천"> 
+
+<meta name="classification" content="">
+<meta name="reply-to" content="">
+<meta name="content-language" content="">
+<meta name="build" content="">
+<meta name="copyright" content="Copyrights &copy; 2017 xn--jx2bxcv35bvwb81kgrbx9lkyi. All rights reserved.">
+
+<meta name="description" content="춘천인테리어필름,인테리어필름,시트지,리폼,시공,가구,싱크대,아파트,상가,모텔,사무실,현관문,방문,샷시,홍천">
+<meta property="og:type" content="website">
+<meta property="og:title" content="춘천인테리어필름">
+<meta property="og:description" content="춘천인테리어필름,인테리어필름,시트지,리폼,시공,가구,싱크대,아파트,상가,모텔,사무실,현관문,방문,샷시,홍천">
+<meta property="og:image" content="http://wmi14.godohosting.com/mobile/images/kakao_d.png">
+<meta property="og:url" content="http://xn--jx2bxcv35bvwb81kgrbx9lkyi.com">
+
+<meta name="twitter:card" content="summary">
+<meta name="twitter:title" content="춘천인테리어필름">
+<meta name="twitter:description" content="춘천인테리어필름,인테리어필름,시트지,리폼,시공,가구,싱크대,아파트,상가,모텔,사무실,현관문,방문,샷시,홍천">
+<meta name="twitter:image" content="http://wmi14.godohosting.com/mobile/images/kakao_d.png">
+<meta name="twitter:url" content="http://xn--jx2bxcv35bvwb81kgrbx9lkyi.com/">
+<link href="http://xn--jx2bxcv35bvwb81kgrbx9lkyi.com/" rel="canonical">
+
+
+<title>춘천인테리어필름</title>
+<script src=/home/godoCommon/admin_inc/js/common.js></script>
+<script src=/home/godoCommon/user_inc/js/prototype.js></script>
+<script src=/home/godoCommon/admin_inc/js/malldiary.js></script>
+<script src="/home/skin/web/basic_C_C/common.js"></script>
+<link rel="styleSheet" href="/home/skin/web/basic_C_C/style.css">
+<style type="text/css">
+.outline_both {
+border-left-width:0;
+border-right-width:0;
+border-left-color:#FFFFFF;
+border-right-color:#FFFFFF;
+}
+
+</style>
+</head>
+
+
+<body bgcolor="" background="" >
+<table width=100% height=100% cellpadding=0 cellspacing=0 border=0>
+<tr>
+<td style="background:url() repeat-x bottom" align=center><script src="/home/skin/web/js/jquery-1.12.4.min.js"></script>
+<script src="/home/skin/web/js/swiper.jquery.min.js"></script>
+<link href="/home/skin/web/js/swiper.css" rel="stylesheet" type="text/css">
+
+<style type="text/css">
+body {background:url(/home/skin/web/img/main/main_bg.png) repeat-x center 0; min-height:100%; width:100%; overflow-x:hidden; } 
+.wrap { width:1920px; margin:0 auto; position:relative; z-index:10; }
+.index .slide_content { position:relative; max-width:1920px; min-width:1080px; height:784px; margin:0 auto; overflow-x:hidden; }
+.index .slide_wrap { position:absolute; left:50%; top:0; width:1920px; margin-left:-960px; text-align:center;  }
+.slide_wrap {overflow:hidden; width:100%; height:100%;}
+.slide_wrap .section {width:1920px; }
+</style>
+
+<div class="container" style="height:37px;">
+	<div class="util">
+		<ul>
+			<li><a href="/home/" alt="홈으로"><img src="/home/skin/web/img/main/util_home.png" alt="홈으로"></a></li>
+			<li><a href="/home/admin" alt="관리자"><img src="/home/skin/web/img/main/util_admin.png" alt="관리자"></a></li>
+			<li><a href="http://blog.naver.com/abba0212" target="_blank" alt="블로그"><img src="/home/skin/web/img/main/util_blog.png" alt="블로그"></a></li>
+		</ul>
+	</div>
+</div>
+<div class="index" id="index">
+    	<div class="slide_content" id="slide_content">
+        	<div class="slide_wrap swiper-container-horizontal swiper-wp8-horizontal" id="slideWrap">
+            		<div class="img_area swiper-wrapper" style="transform: translate3d(-3840px, 0px, 0px); transition-duration: 0ms;">
+                		<div class="swiper-slide swiper-slide-prev" style="width:1920px;" data-swiper-slide-index="0">
+                    			<img class="slide" alt="" src="/home/skin/web/img/main/visual1.png">
+				</div>
+                		<div class="swiper-slide swiper-slide-active" style="width:1920px;" data-swiper-slide-index="1">
+                    			<img class="slide" src="/home/skin/web/img/main/visual2.png">
+                		</div>
+        		</div>
+        	</div>
+	</div>
+
+	<div id="nav">
+	    	<ul>
+			<li class="logo"><a href="/home/" alt="홈"><img src="/home/skin/web/img/main/logo.png" alt="춘천인테리어필름"></a></li>
+	        	<li><a href="/home/contents/main/html.php?htmid=proc/page01.htm">회사소개</a></li>
+            		<li><a href="/home/contents/main/html.php?htmid=proc/page02.htm">인테리어필름</a></li>
+            		<li><a href="/home/contents/board/list.php?id=goodsa">제품소개</a></li>
+			<li><a href="/home/contents/main/html.php?htmid=proc/page03.htm">주거공간&상업공간</a></li>
+            		<li><a href="/home/contents/board/list.php?id=gallery">시공갤러리</a></li>
+            		<li><a href="/home/contents/service/form.php?&idx=1">시공문의</a></li>
+        	</ul>		
+	</div>
+
+	<script>
+            $(function(){
+                var mySwiper = new Swiper('.slide_wrap', {
+                    nextButton: $("#slide_content").find(".btn_right")[0],
+                    prevButton: $("#slide_content").find(".btn_left")[0],
+                    pagination: '.pager',
+                    autoplay: 4000,
+                    loop: true,
+                    autoplayDisableOnInteraction: false,
+                    paginationClickable: true
+                });
+            });
+        </script> 
+</div></td>
+</tr>
+<tr>
+<td height=100% align=center>
+
+<table width=1080 height=100% cellpadding=0 cellspacing=0 border=0 class="outline_both">
+<tr>
+<td valign=top width=100% height=100% bgcolor="" background="" class=outline_side>
+<script language="javascript" src="/home/main/maindiary.js"></script>
+<script src="/home/admin/prototype.js"></script>
+<!-- 메인팝업창 -->
+
+<script language="JavaScript"><!--
+//--></script>
+
+
+
+<div class="container" id="index2">
+	<div id="mainMajor">
+		<img src="/home/skin/web/img/main/tit_service.png" alt="서비스" class="title">
+		<div class="mainService">
+			<ul>
+	            		<li><a href="/home/contents/main/html.php?htmid=proc/page02.htm" alt="인테리어필름"><img src="/home/skin/web/img/main/main_go1.png" alt="인테리어필름"><span class="tx">인테리어필름</span></a></li>
+				<li><a href="/home/contents/main/html.php?htmid=proc/page03.htm" alt="주거공간&상업공간"><img src="/home/skin/web/img/main/main_go2.png" alt="주거공간&상업공간"><span class="tx">주거공간&상업공간</span></a></li>
+				<li><a href="/home/contents/board/list.php?id=gallery" alt="갤러리"><img src="/home/skin/web/img/main/main_go3.png" alt="갤러리"><span class="tx">시공갤러리</span></a></li>
+			</ul>
+		</div>
+		<div class="mainGoods">
+			<ul>
+	            		<li><a href="/home/contents/board/list.php?id=goodsa" alt="basic"><img src="/home/skin/web/img/main/main_good1.png" alt="basic"><span class="tx">basic</span></a></li>
+				<li><a href="/home/contents/board/list.php?id=goodsb" alt="natural"><img src="/home/skin/web/img/main/main_good2.png" alt="natural"><span class="tx">natural</span></a></li>
+				<li><a href="/home/contents/board/list.php?id=goodsc" alt="wood"><img src="/home/skin/web/img/main/main_good3.png" alt="wood"><span class="tx">wood</span></a></li>
+				<li><a href="/home/contents/board/list.php?id=goodsd" alt="outdoor"><img src="/home/skin/web/img/main/main_good4.png" alt="outdoor"><span class="tx">outdoor</span></a></li>
+			</ul>
+		</div>
+		<img src="/home/skin/web/img/main/tit_cs.png" alt="고객센터" class="title">
+		<div class="mainCs">
+			<ul>
+	            		<li><img src="/home/skin/web/img/main/tel.png" alt="010-3093-3900"></li>
+				<li><a href="/home/contents/main/html.php?htmid=proc/page01_02.htm" alt="오시는길"><img src="/home/skin/web/img/main/map.png" alt="오시는길"></a><a href="/home/contents/service/form.php?&idx=1" alt="시공문의"><img src="/home/skin/web/img/main/online.png" alt="시공문의"></a></li>
+				<li><a href="http://blog.naver.com/abba0212" target="_blank" alt="블로그"><img src="/home/skin/web/img/main/blog.png" alt="블로그"></a></li>
+			</ul>
+		</div>
+
+	</div>
+</div>
+
+
+<div style="width:0;height:0;font-size:0"></div></td>
+<td width=0 id=pos_scroll valign=top>
+
+<!-- 스크롤 배너 -->
+
+<div id=scroll style="position:absolute;padding-left:3px; display:none;">
+<table width="74" cellpadding=0 cellspacing=0>
+<tr><td><!-- 맨오른쪽_스크롤배너 (배너관리에서 수정가능) --><img src="./../..//skin/web/basic_C_C/img/banner/quick_01.gif" name="_banner_edit" no="1393" align="absmiddle"></td></tr>
+<tr><td><!-- 맨오른쪽_스크롤배너 (배너관리에서 수정가능) --><img src="./../..//skin/web/basic_C_C/img/banner/quick_02.gif" name="_banner_edit" no="1391" align="absmiddle"></td></tr>
+<tr><td><!-- 맨오른쪽_스크롤배너 (배너관리에서 수정가능) --><img src="./../..//skin/web/basic_C_C/img/banner/quick_03.gif" name="_banner_edit" no="1390" align="absmiddle"></td></tr>
+<tr><td><!-- 맨오른쪽_스크롤배너 (배너관리에서 수정가능) --><img src="./../..//skin/web/basic_C_C/img/banner/quick_04.gif" name="_banner_edit" no="1388" align="absmiddle"></td></tr>
+<tr><td><!-- 맨오른쪽_스크롤배너 (배너관리에서 수정가능) --><a href="#top" target=""><img src="./../..//skin/web/basic_C_C/img/banner/quick_05.gif" name="_banner_edit" no="1386" align="absmiddle"></a></td></tr>
+</table>
+
+</div>
+<!-- 스크롤 배너 활성화 -->
+<script>scrollBanner();</script>
+
+</td>
+</tr>
+</table>
+
+</td>
+</tr>
+<tr>
+<td><div class="mainFooter">
+	<div class="infor">
+		<ul>
+			<li class="copy">상호 : 춘천인테리어필름 &nbsp;&nbsp; &nbsp;&nbsp; 대표 : 유진호 &nbsp;&nbsp; &nbsp;&nbsp; 사업자번호 : 875-41-00260 &nbsp;&nbsp; &nbsp;&nbsp; 대표번호 : 010-3093-3900 &nbsp;&nbsp; &nbsp;&nbsp; 주소 : 강원도 춘천시 퇴계동 909-8 1층</li>
+			<li class="right">Copyright ⓒ 2018 춘천인테리어필름. All rights reserved.</li>
+		</ul>
+	</div>
+</div>
+
+<script>
+	//일정관리 호출!!
+	try{
+		calender("not","f.now");
+	}catch(e){}
+</script></td>
+</tr>
+</table>
+
+<!-- 절대! 지우지마세요 : Start -->
+<iframe name="ifrmHidden" src='/blank.php' style="display:none;width:100%;height:600"></iframe>
+<!-- 절대! 지우지마세요 : End -->
+
+</body>
+</html>
