6a9d997c330caad9932025372a63cabcebbb829f
DESKTOP-7JUBBI6\dhwjd <kmi5063@naver.com>
2023-04-10 22:36:19 +0900

    프로젝트 생성 커밋

---
+++ src/main/java/com/example/hongsong/HongsongApplication.java
@@ -0,0 +1,13 @@
+package com.example.hongsong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongsongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HongsongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hongsong/HongsongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hongsong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HongsongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
